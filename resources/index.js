04d07dadd6a7a72ad94f1a92a949a9db359cffc9
gaya3p <gaya3p13@gmail.com>
2018-03-21 22:00:07 +0530

    added local Storage support

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,9 @@
+// data = localStorage set by previous session or else empty
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
+  todo: [],
+  completed: []
+};
+
 // -------------------------------
 // INPUT BOX ---------------------
 const inputBox = document.getElementById('matter');
@@ -10,50 +16,49 @@ const trashSVG = '<i class="fas fa-trash-alt"></i>';
 const addButton = document.getElementById('add');
 // -------------------------------
 // FUNCTIONS ---------------------
+renderTodoList()
 
+// RENDER LOCAL STORAGE ONLOAD ---
+function renderTodoList() {
+  var todoLength = data.todo.length
+  var completedLength = data.completed.length
 
-// -------------------------------
-// REMOVING AN ITEM --------------
-function removeItem() {
-  var liToBeRemoved = this.parentNode
-  var parent = liToBeRemoved.parentNode
+  if (!todoLength && !completedLength) return //if both are empty, don't render
 
-  parent.removeChild(liToBeRemoved)
-}
-
-// -------------------------------
-// COMPLETING AN ITEM ------------
-function completeItem() {
-  var liToBeCompleted = this.parentNode
-  var parent = liToBeCompleted.parentNode
-  var id = parent.id
-
-  var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo')
-
-  var strikeLine = document.createElement('div')
-  strikeLine.classList.add('strike-line')
-
-  console.log(target.id)
-
-  if (target.id === 'completed') {
-    liToBeCompleted.appendChild(strikeLine)
-  } else if (target.id === 'todo') {
-    var removedChild = liToBeCompleted.querySelector('.strike-line')
-    liToBeCompleted.removeChild(removedChild)
+  for (j = 0; j < todoLength; j++) {
+    var value = data.todo[j]
+    addTodoItem(value)
   }
 
-  parent.removeChild(liToBeCompleted)
-  target.prepend(liToBeCompleted)
-
+  for (c = 0; c < completedLength; c++) {
+    var value = data.completed[c]
+    addTodoItem(value, true)
+  }
 }
 
-
+// UPDATE LOCAL STORAGE ---------
+function dataObjectUpdate() {
+  localStorage.setItem('todoList', JSON.stringify(data))
+}
 
 // -------------------------------
-// ADDING AN ITEM TO THE LIST ----
-const addTodoItem = (value) => {
+// ADDING AN ITEM (1) ------------
+function addItem() {
+  var text = inputBox.value
+  if (text) {
+    addTodoItem(text)
+    inputBox.value = ""
 
-  var list = document.getElementById('todo')
+    data.todo.push(text) // push to data
+  }
+  dataObjectUpdate()
+}
+
+// -------------------------------
+// ADDING AN ITEM TO THE LIST (2)-
+function addTodoItem(value, completed) {
+
+  var list = (completed) ? document.getElementById('completed') : document.getElementById('todo')
 
   var li = document.createElement('li')
   li.innerText = value
@@ -74,17 +79,62 @@ const addTodoItem = (value) => {
   // Add event for removing item
   completeButton.addEventListener('click', completeItem);
   removeButton.addEventListener('click', removeItem);
+
 }
 
 
 
+// -------------------------------
+// COMPLETING AN ITEM ------------
+function completeItem() {
+  var liToBeCompleted = this.parentNode
+  var parent = liToBeCompleted.parentNode
+  var id = parent.id
+  var value = liToBeCompleted.innerText
+  var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo')
+
+  var strikeLine = document.createElement('div')
+  strikeLine.classList.add('strike-line')
+
+  if (id === 'todo') { // parent id = "todo"
+    liToBeCompleted.appendChild(strikeLine)
+
+    data.todo.splice(data.todo.indexOf(value), 1) // push to data
+    data.completed.push(value)
+
+  } else { // parent id = "completed"
+    var removedChild = liToBeCompleted.querySelector('.strike-line')
+    liToBeCompleted.removeChild(removedChild)
+
+    data.completed.splice(data.completed.indexOf(value), 1) // push to data
+    data.todo.push(value)
+  }
+
+  parent.removeChild(liToBeCompleted)
+  target.prepend(liToBeCompleted)
+
+  dataObjectUpdate()
+}
+
+
 
 // -------------------------------
-// ADDING AN ITEM STEP 1 ---------
-const addItem = _ => {
-  var text = inputBox.value
-  if (text) addTodoItem(text)
-  inputBox.value = ""
+// REMOVING AN ITEM --------------
+function removeItem() {
+  var liToBeRemoved = this.parentNode
+  var parent = liToBeRemoved.parentNode
+  var id = parent.id
+  var value = liToBeRemoved.innerText
+
+  if (id === 'todo') { // parent id = "todo"
+    data.todo.splice(data.todo.indexOf(value), 1) // push to data
+  } else { // parent id = "completed"
+    data.completed.splice(data.completed.indexOf(value), 1) // push to data
+  }
+
+  parent.removeChild(liToBeRemoved)
+
+  dataObjectUpdate()
 }
 
 // -------------------------------
