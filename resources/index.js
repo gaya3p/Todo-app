36452dc360d68e8e5d23eafc453c0eebe2049186
gaya3p <gaya3p13@gmail.com>
2018-03-24 19:55:25 +0530

    a lot faster

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,8 @@
+// UPDATE LOCAL STORAGE ---------
+function dataObjectUpdate() {
+  localStorage.setItem('todoList', JSON.stringify(data))
+}
+
 // data = localStorage set by previous session or else empty
 var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
   todo: [],
@@ -9,8 +14,8 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
 const inputBox = document.getElementById('matter');
 
 // SVG CODE FROM FONTWESOME-------
-const checkSVG = '<i class="fas fa-check-circle"></i>';
-const trashSVG = '<i class="fas fa-trash-alt"></i>';
+const checkSVG = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>';
+const trashSVG = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M704 736v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm256 0v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm256 0v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm128 724v-948h-896v948q0 22 7 40.5t14.5 27 10.5 8.5h832q3 0 10.5-8.5t14.5-27 7-40.5zm-672-1076h448l-48-117q-7-9-17-11h-317q-10 2-17 11zm928 32v64q0 14-9 23t-23 9h-96v948q0 83-47 143.5t-113 60.5h-832q-66 0-113-58.5t-47-141.5v-952h-96q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h309l70-167q15-37 54-63t79-26h320q40 0 79 26t54 63l70 167h309q14 0 23 9t9 23z"/></svg>';
 
 // BUTTONS -----------------------
 const addButton = document.getElementById('add');
@@ -36,10 +41,6 @@ function renderTodoList() {
   }
 }
 
-// UPDATE LOCAL STORAGE ---------
-function dataObjectUpdate() {
-  localStorage.setItem('todoList', JSON.stringify(data))
-}
 
 // -------------------------------
 // ADDING AN ITEM (1) ------------
@@ -80,9 +81,28 @@ function addTodoItem(value, completed) {
   completeButton.addEventListener('click', completeItem);
   removeButton.addEventListener('click', removeItem);
 
+  if (completed) addStrikeLine(li, 'completed', true)
 }
 
+// ------------------------------
+// STRIKE LINE ------------------
+function addStrikeLine(item, id, init) {
+  var strikeLine = document.createElement('div')
+  strikeLine.classList.add('strike-line')
 
+  // if it comes from initialisation of Local storage
+  if (init) { 
+    item.appendChild(strikeLine)
+  } else {
+    var line = item.querySelector('.strike-line')
+
+    if (id === 'todo') {
+      item.appendChild(strikeLine)
+    } else {
+      item.removeChild(line)
+    }
+  }
+}
 
 // -------------------------------
 // COMPLETING AN ITEM ------------
@@ -93,19 +113,12 @@ function completeItem() {
   var value = liToBeCompleted.innerText
   var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo')
 
-  var strikeLine = document.createElement('div')
-  strikeLine.classList.add('strike-line')
-
   if (id === 'todo') { // parent id = "todo"
-    liToBeCompleted.appendChild(strikeLine)
-
+    addStrikeLine(liToBeCompleted, id)
     data.todo.splice(data.todo.indexOf(value), 1) // push to data
     data.completed.push(value)
-
   } else { // parent id = "completed"
-    var removedChild = liToBeCompleted.querySelector('.strike-line')
-    liToBeCompleted.removeChild(removedChild)
-
+    addStrikeLine(liToBeCompleted, id)
     data.completed.splice(data.completed.indexOf(value), 1) // push to data
     data.todo.push(value)
   }
@@ -114,9 +127,8 @@ function completeItem() {
   target.prepend(liToBeCompleted)
 
   dataObjectUpdate()
+
 }
-
-
 
 // -------------------------------
 // REMOVING AN ITEM --------------
